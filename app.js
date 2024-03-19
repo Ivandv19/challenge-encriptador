6be472718db4473e026dadda1ae36f2cb1f698e6
Ivanmx19 <ivantrabx10@gmail.com>
2024-03-19 14:25:19 -0600

    actualiza selectores

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+
+
 function validarDatos() {
     var texto = document.getElementById('texto')
+    console.log('validardatos');
+    
 }
 
 function cifrar() {
     var texto = document.getElementById('texto')
+
 }
 
 function descifrar() {
- 
+    var texto = document.getElementById('texto')
+
+}
+
+function asignarTextoCifrado(textoCifrado) {
+   
+   
 }
