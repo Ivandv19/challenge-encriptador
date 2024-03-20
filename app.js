865ab565dafb5a1193b345b572ed9ed947030aec
Ivanmx19 <ivantrabx10@gmail.com>
2024-03-20 13:40:31 -0600

    actualiza html y js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+function botonCifrar() {
+
+    cifrar();
+
+}
+
+
 
 function cifrar() {
-    var texto = document.getElementById('texto').value;
 
+    var texto = document.getElementById('texto').value;
     /*   
     La letra "a" es convertida para "ai"
     La letra "e" es convertida para "enter"
@@ -10,56 +17,51 @@ function cifrar() {
     La letra "u" es convertida para "ufat"*/
 
     //recorremos cada caracter de nuestro texto en busca de las letras y las cambiamos
-
     let arr = [];
-    let arr2 = [];
+    
     for (let i of texto) {
         if (i.includes("a") == true || i.includes("e") == true || i.includes("i") == true || i.includes("o") == true || i.includes("u") == true) {
 
             if (i.includes("a") == true) {
-                arr.push(i);
                 var texto2 = i.replace("a", 'ai');
-                arr2.push(texto2);
+                arr.push(texto2);
             }
 
             if (i.includes("e") == true) {
-                arr.push(i);
                 var texto3 = i.replace("e", 'enter');
-                arr2.push(texto3);
+                arr.push(texto3);
             }
+
             if (i.includes("i") == true) {
-                arr.push(i);
                 var texto4 = i.replace("i", 'imes');
-                arr2.push(texto4);
+                arr.push(texto4);
             }
 
             if (i.includes("o") == true) {
-                arr.push(i);
                 var texto5 = i.replace("o", 'ober');
-                arr2.push(texto5);
+                arr.push(texto5);
             }
 
             if (i.includes("u") == true) {
-                arr.push(i);
                 var texto6 = i.replace("u", 'ofat');
-                arr2.push(texto6);
+                arr.push(texto6);
             }
 
         } else {
             arr.push(i);
-            arr2.push(i);
+
         }
     }
+    
+    textocifrado = arr.join('');
+    return document.getElementById("textoCifrado").value = textocifrado;
+}
 
-    textocifrado = arr2.toString();
-    document.getElementById("textoCifrado").value = textocifrado;
-
+function botonDescifrar() {
 
 }
 
-
-
-function descifrar() {
+function descifrar(texto) {
 
 }
 
