eb4bdbf671531a6f0cb33cc19861a0a6e5200dda
fluxdev19 <ivangtx19@gmail.com>
2024-07-31 21:27:50 -0600

    refactor: Refactoriza código para mejor legibilidad

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
 // Función para validar el texto ingresado antes de encriptar
-function validarInput() {
-    const input = document.getElementById('texto').value.trim(); // Obtener el valor del texto ingresado y eliminar espacios en blanco
+function validarInput(text) {
     const regex = /^[a-z\s]*$/; // Expresión regular para permitir solo letras minúsculas y espacios
-    if (!regex.test(input) || input === "") { // Si el texto no coincide con la expresión regular o está vacío
-        alert('El texto ingresado no es válido. Por favor, use solo letras minúsculas sin acentos ni caracteres especiales.'); // Mostrar mensaje de alerta
+    if (!regex.test(text) || text === "") {
+        // Si el texto no coincide con la expresión regular o está vacío
+        alert( "El texto ingresado no es válido. Por favor, use solo letras minúsculas sin acentos ni caracteres especiales."); // Mostrar mensaje de alerta
         return false; // Devolver falso
-    } else { // Si el texto es válido
-        encriptar(); // Llamar a la función para encriptar
+    } else {
         return true; // Devolver verdadero
     }
 }
 
 // Función para encriptar el texto
 function encriptar() {
-    const texto = document.getElementById('texto').value; // Obtener el valor del texto ingresado
-    const arr = texto.split(''); // Convertir el texto en un array de caracteres
-
-    for (let i = 0; i < arr.length; i++) { // Recorrer cada caracter del texto
-        if ('aeiou'.includes(arr[i])) { // Si el caracter es una vocal
-            switch (arr[i]) { // Reemplazar cada vocal por su correspondiente encriptado
-                case 'a':
-                    arr[i] = 'ai';
-                    break;
-                case 'e':
-                    arr[i] = 'enter';
-                    break;
-                case 'i':
-                    arr[i] = 'imes';
-                    break;
-                case 'o':
-                    arr[i] = 'ober';
-                    break;
-                case 'u':
-                    arr[i] = 'ufat';
-                    break;
+    const input = document.getElementById("texto").value.trim(); // Obtener el valor del texto ingresado y eliminar espacios en blanco
+    if (validarInput(input)) {
+        const arr = input.split(""); // Convertir el texto en un array de caracteres
+        for (let i = 0; i < arr.length; i++) {
+            // Recorrer cada caracter del texto
+            if ("aeiou".includes(arr[i])) {
+                // Si el caracter es una vocal
+                switch (arr[i]) {
+                    case "a":
+                        arr[i] = "ai";
+                        break;
+                    case "e":
+                        arr[i] = "enter";
+                        break;
+                    case "i":
+                        arr[i] = "imes";
+                        break;
+                    case "o":
+                        arr[i] = "ober";
+                        break;
+                    case "u":
+                        arr[i] = "ufat";
+                        break;
+                }
             }
         }
-    }
 
-    const textoCifrado = arr.join(''); // Unir los caracteres encriptados para formar el texto cifrado
-    console.log(textoCifrado); // Mostrar el texto cifrado en la consola
-    document.getElementById("texto2").value = textoCifrado; // Establecer el texto cifrado en el segundo textarea
-    document.getElementById('container-info').style.display='none';
-    document.getElementById("container-info2").style.display = "flex"; // Mostrar el contenedor del segundo textarea
-    document.getElementById("container-botoncopiar").style.display = 'flex';
+        const textoCifrado = arr.join(""); // Unir los caracteres encriptados para formar el texto cifrado
+        document.getElementById("container-info").style.display = "none";
+        document.getElementById("container-info2").style.display = "flex"; // Mostrar el contenedor del segundo textarea
+        document.getElementById("texto2").value = textoCifrado; // Establecer el texto cifrado en el segundo textarea
+        document.getElementById("container-botoncopiar").style.display = "flex";
+    } else {
+        alert("error al encriptar texto");
+    }
 }
 
 // Función para desencriptar el texto
 function desencriptar() {
-    const textoCifrado = document.getElementById('texto').value; // Obtener el texto cifrado
+    const textoCifrado = document.getElementById("texto").value.trim(); // Obtener el texto cifrado
     const textoDescifrado = textoCifrado // Desencriptar el texto reemplazando cada encriptación por su vocal original
         .replace(/ai/g, "a")
         .replace(/enter/g, "e")
@@ -56,18 +59,19 @@ function desencriptar() {
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
 
-    console.log(textoDescifrado); // Mostrar el texto descifrado en la consola
+    document.getElementById("container-info").style.display = "none";
+    document.getElementById("container-info2").style.display = "flex"; // Mostrar el contenedor del segundo textarea
     document.getElementById("texto2").value = textoDescifrado; // Establecer el texto descifrado en el segundo textarea
 }
 
-// Función para copiar el texto cifrado al portapapeles
-function copiarTexto() {
+// Función para copiar el texto cifrado al portapapeles utilizando async/await
+async function copiarTexto() {
     const textoCifrado = document.getElementById("texto2").value; // Obtener el texto cifrado
-    navigator.clipboard.writeText(textoCifrado) // Copiar el texto cifrado al portapapeles utilizando la API del navegador
-        .then(function() {
-            alert("Texto copiado al portapapeles correctamente"); // Mostrar mensaje de éxito
-        })
-        .catch(function(error) {
-            console.error("Error al copiar texto: ", error); // Mostrar mensaje de error en la consola
-        });
+    try {
+        await navigator.clipboard.writeText(textoCifrado); // Intentar copiar el texto al portapapeles
+        alert("Texto copiado al portapapeles correctamente"); // Mostrar mensaje de éxito
+    } catch (error) {
+        console.error("Error al copiar texto: ", error); // Mostrar mensaje de error en la consola
+        alert("No se pudo copiar el texto al portapapeles. Inténtalo nuevamente."); // Mostrar mensaje de error al usuario
+    }
 }
